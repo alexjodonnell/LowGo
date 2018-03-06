7c2b2563f9c68f5bf7104c1517fc4605e0b68b3e
alexodonn <alexodonn@gmail.com>
2018-03-05 22:32:02 -0400

    zbuffer triangle method so shapes can be properly rasterized

---
+++ render/RenderUtils.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alex O'Donnell on 2018-03-05.
+//
+
+#include "renderer.h"
---
+++ render/RenderUtils.h
@@ -0,0 +1,8 @@
+//
+// Created by Alex O'Donnell on 2018-03-05.
+//
+
+#ifndef LOWGO_RENDERER_H
+#define LOWGO_RENDERER_H
+
+#endif //LOWGO_RENDERER_H
